baea151f016682d41331bd94469591bc4a2c7112
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-22 15:58:58 +0300

    herpers implemented

---
+++ app/helpers/shopping_engine/order_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module OrderHelper
+  def current_sort
+    sort_present? ? params[:order].to_s.humanize : I18n.t('general.all')
+  end
+
+  def sort_present?
+    return unless params.key? :order
+    Order::MY_ORDERS_STATES.include? params[:order].to_sym
+  end
+end

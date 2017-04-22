5cefab5441b06972285bc1886a5514598ecb130b
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-22 12:44:14 +0300

    routes implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 ShoppingEngine::Engine.routes.draw do
+  resources :order_items, except: %i(show, new, edit)
+  resources :coupons, only: :create
+  resources :orders do
+    get 'confirm', on: :member
+  end
+  resources :order_steps, only: %i(show update)
 end

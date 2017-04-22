174d722c53500e5410f489cf9062ef09e77cfabe
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-22 13:29:35 +0300

    engine requirments added

--- lib/shopping_engine.rb
+++ lib/shopping_engine.rb
@@ -1,5 +1,22 @@
-require "shopping_engine/engine"
+require 'shopping_engine/engine'
+
+require 'aasm'
+require 'bootstrap-sass'
+require 'cancancan'
+require 'coffee-rails'
+require 'countries'
+require 'country_select'
+require 'devise'
+require 'draper'
+require 'font-awesome-rails'
+require 'haml'
+require 'jquery-rails'
+require 'omniauth'
+require 'omniauth-facebook'
+require 'sass-rails'
+require 'simple_form'
+require 'wicked'
 
 module ShoppingEngine
-  # Your code goes here...
+
 end
